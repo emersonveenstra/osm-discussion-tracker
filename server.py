cef8c952edb7962472673a23d813e6cc000133b0
emerson <git@emersonveenstra.net>
2023-11-23 08:02:22 -0500

    split backend into utils and server.py

--- server.py
+++ server.py
@@ -2,6 +2,11 @@ import os
 import psycopg
 import strawberry
 import typing
+from datetime import datetime
+from fastapi import FastAPI, Response
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from strawberry.fastapi import GraphQLRouter
 
 from datetime import datetime
 from psycopg.rows import dict_row
@@ -173,3 +178,51 @@ class Query:
 	get_changeset_details: "FullChangeset" = strawberry.field(resolver=get_changeset_details)
 
 schema = strawberry.Schema(query=Query)
+
+graphql_app = GraphQLRouter(schema)
+
+app = FastAPI()
+
+class Resolve(BaseModel):
+	uid: int = 0
+	csid: int = 0
+	status: str
+	expiresAt: object | None = None
+
+@app.post("/resolve", status_code=200)
+async def resolve(resolve: Resolve, response: Response):
+	if (resolve.uid == 0 or resolve.csid == 0):
+		response.status_code = 400
+		return response
+	resolved_at = datetime.utcnow()
+	with conn.cursor() as curs:
+		is_existing = curs.execute('select * from odt_resolved where uid=%s and csid=%s', (resolve.uid, resolve.csid)).fetchone()
+		if is_existing:
+			if resolve.expiresAt:
+				curs.execute('update odt_resolved set expires_at = %s where uid=%s and csid=%s', (resolve.expiresAt, resolve.uid, resolve.csid))
+			else:
+				curs.execute('update odt_resolved set expires_at = null where uid=%s and csid=%s', (resolve.uid, resolve.csid))
+		else:
+			curs.execute('insert into odt_resolved (uid, csid, resolved_at, expires_at) values (%s,%s,%s,%s)', (resolve.uid, resolve.csid, resolved_at, resolve.expiresAt))
+		conn.commit()
+	return {"message": resolve}
+
+@app.post("/unresolve", status_code=200)
+async def resolve(unresolve: Resolve, response: Response):
+	if (unresolve.uid == 0 or unresolve.csid == 0):
+		response.status_code = 400
+		return response
+	with conn.cursor() as curs:
+		curs.execute('delete from odt_resolved where uid=%s and csid=%s', (unresolve.uid, unresolve.csid))
+		conn.commit()
+	return {"message": f"{unresolve.uid} has unresolved {unresolve.csid}"}
+
+app.include_router(graphql_app, prefix="/graphql")
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=["*"],
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
--- utils/changeset-importer.py
+++ utils/changeset-importer.py
@@ -24,53 +24,53 @@ def doReplication(first_state, last_state, step=1):
 		stateString = str(state)
 		urlState = f'00{stateString[0]}/{stateString[1:4]}/{stateString[4:7]}'
 		url = f'https://planet.openstreetmap.org/replication/changesets/{urlState}.osm.gz'
-		with urllib.request.urlopen(url, timeout=10) as f:
-			osmfile = gzip.open(BytesIO(f.read()))
-			print(f'importing {urlState}')
-			context = etree.iterparse(osmfile)
-			next(context)
-			for action, elem in context:
-				if(elem.tag != 'changeset'):
-					continue
+		f = urllib.request.urlopen(url, timeout=10)
+		osmfile = gzip.open(BytesIO(f.read()))
+		print(f'importing {urlState}')
+		context = etree.iterparse(osmfile)
+		next(context)
+		for action, elem in context:
+			if(elem.tag != 'changeset'):
+				continue
 
-				if (elem.attrib.get('closed_at', False) == False):
-					continue
+			if (elem.attrib.get('closed_at', False) == False):
+				continue
 
-				csid = int(elem.attrib['id'])
-				uid = elem.attrib.get('uid', '0')
-				username = elem.attrib.get('user', 'unknown')
-				ts = elem.attrib.get('closed_at')
-				cs_comment = ""
-				for tag in elem.iterchildren(tag='tag'):
-					if tag.attrib['k'] == "comment":
-						cs_comment = tag.attrib["v"]
+			csid = int(elem.attrib['id'])
+			uid = elem.attrib.get('uid', '0')
+			username = elem.attrib.get('user', 'unknown')
+			ts = elem.attrib.get('closed_at')
+			cs_comment = ""
+			for tag in elem.iterchildren(tag='tag'):
+				if tag.attrib['k'] == "comment":
+					cs_comment = tag.attrib["v"]
 
-				if csid not in changesets:
-					changesets[csid] = {
-						"uid": uid,
-						"username": username,
-						"ts": ts,
-						"cs_comment": cs_comment
-					}
+			if csid not in changesets:
+				changesets[csid] = {
+					"uid": uid,
+					"username": username,
+					"ts": ts,
+					"cs_comment": cs_comment
+				}
 
 
-				for discussion in elem.iterchildren(tag='discussion'):
-					for commentElement in discussion.iterchildren(tag='comment'):
-						comment_uid = commentElement.attrib.get('uid', '0')
-						comment_username = commentElement.attrib.get('user', 'unknown')
-						comment_ts = commentElement.attrib.get('date')
-						for text in commentElement.iterchildren(tag='text'):
-							comment_text = text.text
-						hashtext = f'{csid}{comment_uid}{comment_ts}'
-						comment_id = sha1(hashtext.encode()).hexdigest()
-						if comment_id not in comments:
-							comments[comment_id] = {
-								"csid": csid,
-								"comment_uid": comment_uid,
-								"comment_username": comment_username,
-								"comment_ts": comment_ts,
-								"comment_text": comment_text
-							}
+			for discussion in elem.iterchildren(tag='discussion'):
+				for commentElement in discussion.iterchildren(tag='comment'):
+					comment_uid = commentElement.attrib.get('uid', '0')
+					comment_username = commentElement.attrib.get('user', 'unknown')
+					comment_ts = commentElement.attrib.get('date')
+					for text in commentElement.iterchildren(tag='text'):
+						comment_text = text.text
+					hashtext = f'{csid}{comment_uid}{comment_ts}'
+					comment_id = sha1(hashtext.encode()).hexdigest()
+					if comment_id not in comments:
+						comments[comment_id] = {
+							"csid": csid,
+							"comment_uid": comment_uid,
+							"comment_username": comment_username,
+							"comment_ts": comment_ts,
+							"comment_text": comment_text
+						}
 			elem.clear()
 
 	with conn.cursor() as curs:
@@ -84,10 +84,13 @@ def doReplication(first_state, last_state, step=1):
 			existing_comment_ids.append(existing_comment_id["comment_id"])
 		for csid, data in changesets.items():
 			if csid not in existing_csids:
-				curs.execute("insert into odt_changeset (csid, uid, ts, username, comment) values (%s, %s, %s, %s, %s)", (csid, data["uid"], data["ts"], data["username"], data["cs_comment"]))
+				curs.execute("insert into odt_changeset (csid, uid, ts, username, comment, last_activity) values (%s, %s, %s, %s, %s,%s)", (csid, data["uid"], data["ts"], data["username"], data["cs_comment"], data["ts"]))
 		for comment_id, data in comments.items():
 			if comment_id not in existing_comment_ids:
 					curs.execute("insert into odt_comment (comment_id, csid, uid, ts, username, comment) values (%s, %s, %s, %s, %s, %s)", (comment_id, data["csid"], data["comment_uid"], data["comment_ts"], data["comment_username"], data["comment_text"]))
+					cs_activity = curs.execute('select last_activity from odt_changeset where csid=%s', (data["csid"],)).fetchone()
+					if not cs_activity["last_activity"] or data["comment_ts"] > cs_activity["last_activity"]:
+						curs.execute('update odt_changeset set last_activity=%s where csid=%s', (data["comment_ts"], data["csid"]))
 		conn.commit()
 		
 	print(f'finished importing {len(changesets)} changesets and {len(comments)} comments')
@@ -110,21 +113,10 @@ def doCron(limit = 1000):
 			else:
 				print(f'{planet_current_state} is the last state file available')
 
-def doBackfill(number_to_backfill):
-	with conn.cursor() as curs:
-		curs.execute('select min_state from odt_state')
-		min_state = curs.fetchone()["min_state"]
-		new_min_state = min_state - number_to_backfill
-		print(f'backfilling state files {min_state-1} to {new_min_state}')
-		doReplication(min_state-1, new_min_state-1, -1)
-		curs.execute('update odt_state set min_state = %s', (new_min_state,))
-		conn.commit()
-
 
 argparser = argparse.ArgumentParser(description="Import OSM Changeset replication files")
 argparser.add_argument('-r', '--replication', action='store', dest='replication', type=int, help='OSM replication state number to import')
 argparser.add_argument('-c', '--cron', action='store', dest='isCron', nargs='?', const=1000, type=int, help='cron mode to import the next number of state files, limited by the number specified (default limit 1000)')
-argparser.add_argument('-b', '--backfill', action='store', dest='toBackfill', nargs='?', const=1000, type=int, help='number of state files to backfill (default 1000)')
 
 args = argparser.parse_args()
 
@@ -132,5 +124,3 @@ if (args.replication):
 	doReplication(args.replication, args.replication+1)
 if (args.isCron):
 	doCron(args.isCron)
-if (args.toBackfill):
-	doBackfill(args.toBackfill)
