5c4931018122d19e2c6323f0cf2bd175de0e5c77
emerson <git@emersonveenstra.net>
2024-01-19 17:17:03 -0500

    fix unsnooze script

--- utils/unsnooze.py
+++ utils/unsnooze.py
@@ -2,3 +2,4 @@ import changeset_importer
 
 with changeset_importer.conn.cursor() as curs:
 	curs.execute('update odt_watched_changesets set snooze_until = null where snooze_until < now()')
+	changeset_importer.conn.commit()
